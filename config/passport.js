0755e22de85af4609be56cc34fc6f6d30eff9978
AfeedaKN <afeedaafeedakn@gmail.com>
2025-10-07 15:54:57 +0530

    google aut7

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ passport.use(
     { 
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://www.fitvibe.world/google/auth/callback/",
+      callbackURL: "https://www.fitvibe.world/google/auth/callback",
       passReqToCallback: true,
     },
     async (accessToken, refreshToken, profile, done) => {
